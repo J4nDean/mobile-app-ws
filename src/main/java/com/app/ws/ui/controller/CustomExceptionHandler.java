74f709a1ba628c0930fe8a00e9e805bd27a51101
J4nDean <jandziekan333@gmail.com>
2024-01-19 20:18:34 +0100

    initial commit

---
+++ src/main/java/com/app/ws/ui/controller/CustomExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.app.ws.ui.controller;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.http.ResponseEntity;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@ControllerAdvice
+public class CustomExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Object> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        List<String> errors = ex.getBindingResult()
+                .getAllErrors()
+                .stream()
+                .map(error -> error.getDefaultMessage())
+                .collect(Collectors.toList());
+
+        return ResponseEntity.badRequest().body(errors);
+    }
+}
